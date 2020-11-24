1bf44df209121cf369baf83944b165e241b338c7
Nat Troyer <nathanaelatroyer@gmail.com>
2020-11-24 09:48:07 -0500

    fixed an error where login bar was being called at program start

--- src/components/login/login.component.js
+++ src/components/login/login.component.js
@@ -20,6 +20,14 @@ const WelcomeInput = styled.p`
 `;
 
 export default class Login extends Component {
+    constructor(props) {
+        super();
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
+        this.newaccount = this.newaccount.bind(this);
+    }
+
     newaccount() {
         this.props.onNewAccount();
     }
@@ -37,15 +45,15 @@ export default class Login extends Component {
             return(
                 <WrapperDiv>
                     <WelcomeInput>Login to save your images!</WelcomeInput>
-                    <Button variant="primary" onClick={this.login()}>Login</Button>
-                    <Button variant="primary" onClick={this.newaccount()}>Create Account</Button>
+                    <Button variant="primary" onClick={this.login}>Login</Button>
+                    <Button variant="primary" onClick={this.newaccount}>Create Account</Button>
                 </WrapperDiv>
             )
         } else {
             return(
                 <WrapperDiv>
                     <WelcomeInput>Welcome, {this.props.username}</WelcomeInput>
-                    <Button variant="primary" onClick={this.logout()}>Logout</Button>
+                    <Button variant="primary" onClick={this.logout}>Logout</Button>
                 </WrapperDiv>
             )
         }
