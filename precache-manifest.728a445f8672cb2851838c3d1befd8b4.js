1edfbada2054d9bd60b2baec89d5f0d1090f83a7
Nat Troyer <nathanaelatroyer@gmail.com>
2021-07-10 15:33:05 -0400

    Updates

---
+++ precache-manifest.728a445f8672cb2851838c3d1befd8b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25bbcb6778717f1523170463c5549b06",
+    "url": "/PolyominoPainter/index.html"
+  },
+  {
+    "revision": "e6e7b94e19e54f40e2af",
+    "url": "/PolyominoPainter/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "5bca9a414a9b511a01b7",
+    "url": "/PolyominoPainter/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "e6e7b94e19e54f40e2af",
+    "url": "/PolyominoPainter/static/js/2.20393885.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PolyominoPainter/static/js/2.20393885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bca9a414a9b511a01b7",
+    "url": "/PolyominoPainter/static/js/main.a35e9e1e.chunk.js"
+  },
+  {
+    "revision": "6a3c25bf7a1acf733ae1",
+    "url": "/PolyominoPainter/static/js/runtime-main.0923c59f.js"
+  }
+]);
